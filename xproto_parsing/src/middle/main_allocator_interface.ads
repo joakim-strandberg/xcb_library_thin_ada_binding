5c14c345b62ac29a3b65b7c5c6832e5973bc00f2
Joakim Strandberg <joakimds@kth.se>
2017-02-05 14:14:31 +0100

    Bugfix

--- xproto_parsing/src/middle/main_allocator_interface.ads
+++ xproto_parsing/src/middle/main_allocator_interface.ads
@@ -75,106 +75,106 @@ package Main_Allocator_Interface is
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Xcb        : X_Proto_XML.Xcb.Ptr) return Current_Tag.Ptr;
+                             Xcb        : X_Proto_XML.Xcb.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Struct     : X_Proto_XML.Struct.Ptr) return Current_Tag.Ptr;
+                             Struct     : X_Proto_XML.Struct.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             X_Id       : X_Proto_XML.X_Id.Ptr) return Current_Tag.Ptr;
+                             X_Id       : X_Proto_XML.X_Id.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             X_Id_Union : X_Proto_XML.X_Id_Union.Ptr) return Current_Tag.Ptr;
+                             X_Id_Union : X_Proto_XML.X_Id_Union.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag      : Current_Tag.Ptr;
-                             Type_Definition : X_Proto_XML.Type_Definition.Ptr) return Current_Tag.Ptr;
+                             Type_Definition : X_Proto_XML.Type_Definition.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Enum       : X_Proto_XML.Enum.Ptr) return Current_Tag.Ptr;
+                             Enum       : X_Proto_XML.Enum.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Event      : X_Proto_XML.Event.Ptr) return Current_Tag.Ptr;
+                             Event      : X_Proto_XML.Event.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Event_Copy : X_Proto_XML.Event_Copy.Ptr) return Current_Tag.Ptr;
+                             Event_Copy : X_Proto_XML.Event_Copy.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Union      : X_Proto_XML.Union.Ptr) return Current_Tag.Ptr;
+                             Union      : X_Proto_XML.Union.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Error      : X_Proto_XML.Error.Ptr) return Current_Tag.Ptr;
+                             Error      : X_Proto_XML.Error.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Error_Copy : X_Proto_XML.Error_Copy.Ptr) return Current_Tag.Ptr;
+                             Error_Copy : X_Proto_XML.Error_Copy.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Request    : X_Proto_XML.Request.Ptr) return Current_Tag.Ptr;
+                             Request    : X_Proto_XML.Request.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Field      : X_Proto_XML.Field.Ptr) return Current_Tag.Ptr;
+                             Field      : X_Proto_XML.Field.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Pad        : X_Proto_XML.Pad.Ptr) return Current_Tag.Ptr;
+                             Pad        : X_Proto_XML.Pad.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             List       : X_Proto_XML.List.Ptr) return Current_Tag.Ptr;
+                             List       : X_Proto_XML.List.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Kind       : X_Proto_XML.Type_P.Ptr) return Current_Tag.Ptr;
+                             Kind       : X_Proto_XML.Type_P.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Item       : X_Proto_XML.Item.Ptr) return Current_Tag.Ptr;
+                             Item       : X_Proto_XML.Item.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag    : Current_Tag.Ptr;
-                             Documentation : X_Proto_XML.Documentation.Ptr) return Current_Tag.Ptr;
+                             Documentation : X_Proto_XML.Documentation.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Operation  : X_Proto_XML.Operation.Ptr) return Current_Tag.Ptr;
+                             Operation  : X_Proto_XML.Operation.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Value      : X_Proto_XML.Value_Access_Type) return Current_Tag.Ptr;
+                             Value      : X_Proto_XML.Value_Access_Type) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag      : Current_Tag.Ptr;
-                             Field_Reference : X_Proto_XML.Field_Reference_Access_Type) return Current_Tag.Ptr;
+                             Field_Reference : X_Proto_XML.Field_Reference_Access_Type) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             See        : X_Proto_XML.See.Ptr) return Current_Tag.Ptr;
+                             See        : X_Proto_XML.See.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Example    : X_Proto_XML.Example.Ptr) return Current_Tag.Ptr;
+                             Example    : X_Proto_XML.Example.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag  : Current_Tag.Ptr;
-                             Value_Param : X_Proto_XML.Value_Param.Ptr) return Current_Tag.Ptr;
+                             Value_Param : X_Proto_XML.Value_Param.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag : Current_Tag.Ptr;
-                             Reply      : X_Proto_XML.Reply.Ptr) return Current_Tag.Ptr;
+                             Reply      : X_Proto_XML.Reply.Ptr) return Current_Tag.Ptr is abstract;
 
    function New_Current_Tag (This       : T;
                              Parent_Tag       : Current_Tag.Ptr;
-                             Expression_Field : X_Proto_XML.Expression_Field.Ptr) return Current_Tag.Ptr;
+                             Expression_Field : X_Proto_XML.Expression_Field.Ptr) return Current_Tag.Ptr is abstract;
 
 end Main_Allocator_Interface;
