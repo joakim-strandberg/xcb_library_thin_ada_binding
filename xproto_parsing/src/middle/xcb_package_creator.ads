177e1c8be2a52847e6a5c77225d7eee49c270a3e
Joakim Strandberg <joakimds@kth.se>
2017-02-05 16:39:37 +0100

    Removed unused files

--- xproto_parsing/src/middle/xcb_package_creator.ads
+++
@@ -1,9 +0,0 @@
-with X_Proto_XML;
-with Basic_Bounded_Dynamic_Pools;
-
-package XCB_Package_Creator is
-
-   procedure Create_XCB_Package (XCB  : X_Proto_XML.Xcb.T;
-                                 Pool : in out Basic_Bounded_Dynamic_Pools.Basic_Dynamic_Pool);
-
-end XCB_Package_Creator;
